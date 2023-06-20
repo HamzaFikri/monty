591d16a14151ad9c3d1c21da1fd8344c1b34d584
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-06-20 14:44:20 -0700

    Header Files

--- monty.h
+++ monty.h
@@ -1,25 +1,23 @@
-#ifndef _MONTY_H_
-#define _MONTY_H_
+#ifndef MONTY_H
+#define MONTY_H
 
-#include <math.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+/* System Libraries*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <stddef.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
-/*extern variable, stack or queue*/
-
-extern char *flag;
-
-#define BUF_LENGTH 1024
-
+/* Structs */
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
+ *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
@@ -29,11 +27,11 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-
 /**
- * struct instruction_s - opcoode and its function
+ * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
+ *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
@@ -42,55 +40,44 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+/**
+ * struct line_list_s - lines amount
+ * @str: the line
+ * @prev: points to the previous element of the list
+ * @next: points to the next element of the list
+ * @line_n: new line?
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct line_list_s
+{
+	char *str;
+	int line_n;
+	struct line_list_s *prev;
+	struct line_list_s *next;
+} line_list_t;
+/**
+ * struct error_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct error_s
+{
+	char *opcode;
+	void (*f)(line_list_t *node, FILE *f);
+} error_t;
 
 
 
-/*basic functions related to doubly linked list*/
-stack_t *add_node(stack_t **head, const int n);
-void free_stack(stack_t *head);
-stack_t *pop_s(stack_t **head);
-stack_t *dequeue(stack_t **head);
-stack_t *add_node_end(stack_t **head, int n);
+/* Our Libraries */
+#include "aux_funcs.h"
 
-/*functions to print the stack or queue*/
-void pall(stack_t **h, unsigned int l);
-void pstr(stack_t **h, unsigned int l);
-void pchar(stack_t **h, unsigned int l);
-void pint(stack_t **h, unsigned int l);
 
-/*in push_and_pop*/
-void pop(stack_t **h, unsigned int l);
-int push(stack_t **h, char *line, unsigned int l);
-
-/*in move_elements_functions*/
-void swap(stack_t **h, unsigned int l);
-void rotl(stack_t **h, unsigned int l);
-void rotr(stack_t **h, unsigned int l);
-
-/*in calculations*/
-int get_argument(stack_t **h, char *opcode, unsigned int l);
-void _add(stack_t **h, unsigned int l);
-void _sub(stack_t **h, unsigned int l);
-void _div(stack_t **h, unsigned int l);
-void _mul(stack_t **h, unsigned int l);
-void _mod(stack_t **h, unsigned int l);
-
-/*in nopandqueue*/
-void stack(stack_t **h, unsigned int l);
-void queue(stack_t **h, unsigned int l);
-void nop(stack_t **h, unsigned int l);
-
-/*in helpers*/
-char *skip_spaces(char *s);
-char *reach_number(char *s);
-int _strcmp(char *s1, char *s2);
-int _strncmp(char *s1, char *s2, int n);
-int _strlen(char *s);
-
-/* in getline.c */
-ssize_t _getline(char **buf, size_t *size, int file_strm);
-
-/*in execute*/
-int execute(stack_t **h, char *line, unsigned int line_number);
+/* Global Var to store (Stack Head) */
+extern stack_t *stack_h;
 
 #endif
